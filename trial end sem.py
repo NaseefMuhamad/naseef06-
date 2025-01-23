7eea2b7ac879d13ff7f142317dc055babd84381b
NaseefMuhamad <naseefmuhamad277@gmail.com>
2025-01-23 16:42:26 +0300

    Add files via upload

---
+++ trial end sem.py
@@ -0,0 +1,62 @@
+from matplotlib import units
+
+
+
+
+# def yaka_units(amount):
+#     received=amount/560
+#     return (received)
+# def water_units(consumed):
+#     payed=consumed*4100
+#     return(payed)
+# def main():
+#     print(f" choose either yaka units or water units")
+#     print(f"1: The yaka units")
+#     print(f"2: The water units")
+#     choice=input("Entre your choice (1 or 2)")
+#     if choice=='1':
+#         amount=int(input("Entre the amount paid by the customer"))
+#         received=amount/560
+#         print(f" the yaka units received by a customer are {received}")
+#     elif choice=='2':
+#         consumed=int(input("entre units conmsumed by customer"))
+#         payed=consumed*4100
+#         print(f" the amount payed for water units is {payed} in shs") 
+#     else:
+#         print(" invalid choice")
+    
+# if __name__ == "__main__":
+#     main()
+
+   
+# for x in range(1,13):
+#     for y in range(1,13):
+#         print(f" {x}*{y}={x*y}") 
+
+# secret_number=29
+# guess_count=0
+# guess_limit=3
+# while guess_count<guess_limit:
+#     guess=int(input(" make your guess"))
+#     guess_count+=1
+#     if guess==secret_number:
+#         print("you won")
+#         break
+#     else:
+#         print('you lost')
+
+# def ass_stud(assignments):
+#     assignments=sum(assignments)/len(assignments)
+#     print(f" the average is {assignments}")
+#     return assignments
+# my_assignments=[60,70,80,90]
+# ass_stud(my_assignments)
+
+# def area_circle(R,P):
+#     area=P*R**2
+#     print(f" the area is {area}")
+#     return area
+# area_circle(2,3.142)
+ 
+
+    
