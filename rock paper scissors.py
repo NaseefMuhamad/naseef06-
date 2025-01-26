d3a5ffd9b7b2431ca2d23184f215d710ef5994dc
NaseefMuhamad <naseefmuhamad277@gmail.com>
2025-01-26 15:47:26 +0300

    Add files via upload

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -3,25 +3,28 @@ import random
 emojis={"r":"🪨","p":"📃","s":"✂️"}
 choices=('r','p','s')
 
-user_choice=input("rock,paper,or scissors?(r/p/s): ").lower()
-if user_choice not in choices:
-    print("invalid choice!")
 
-computer_choice=random.choice(choices)
+while True:
+    user_choice=input("rock,paper,or scissors?(r/p/s): ").lower()
+    if user_choice not in choices:
+        print("invalid choice!")
+        continue
 
-print(f"you chose {emojis[user_choice]}")
-print(f"computer chose {emojis[computer_choice]}")    
+    computer_choice=random.choice(choices)
 
-if user_choice==computer_choice:
-    print("tie")
-elif (
-    (user_choice=='r' and computer_choice=='s') or \
-    (user_choice=='s' and computer_choice=='p') or \
-    (user_choice=='p'and computer_choice=='r')):
-    print("you win")
-else:
-    print("you lost")
-    
-should_continue=input('continue?(y/n):').lower()
-if should_continue =='n':
-    print
+    print(f"you chose {emojis[user_choice]}")
+    print(f"computer chose {emojis[computer_choice]}")    
+
+    if user_choice==computer_choice:
+        print("tie")
+    elif (
+        (user_choice=='r' and computer_choice=='s') or 
+        (user_choice=='s' and computer_choice=='p') or 
+        (user_choice=='p'and computer_choice=='r')):
+        print("you win")
+    else:
+        print("you lost")
+        
+    should_continue=input('continue?(y/n):').lower()
+    if should_continue =='n':
+        break
