8431209cfba105591a228de7a8e9c3458c3ac6fd
NaseefMuhamad <naseefmuhamad277@gmail.com>
2025-01-27 17:27:59 +0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,19 @@
+/*
+void and returning functions
+*/
+/*
+function addition(numberone,numbertwo){
+    return numberone+numbertwo
+}
+function sum(numberone,numbertwo){
+    let summation =numberone +numbertwo;
+    console.log(summation)
+}
+sum(6,8);
+console.log(addition(32,67));
+*/
+
+const welcome=(nameone) =>{
+    console.log(' hi, ${nameone} ');
+}
+welcome("naseef");
