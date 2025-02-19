5e540885b0ee1bb96fc54d23356793c0797f0f7a
NaseefMuhamad <naseefmuhamad277@gmail.com>
2025-02-19 09:00:19 +0300

    Add files via upload

---
+++ page.js
@@ -0,0 +1,13 @@
+export default function Home(){
+  let first=923;
+  let second=37;
+  let sum=first+second
+  
+  return (
+    <div>
+    <h1>A CORPORATIONS</h1>
+    <p>Our company is a leading provider of innovative solutions in the field of technology and software</p>
+    <strong>{sum}</strong>
+  </div>
+  )
+}
